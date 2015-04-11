be24ff535ce1bf3ce8bd0223df1592545565e7e8
Ismail <xd4rker@gmail.com>
2015-04-11 12:23:46 +0100

    Update popular.php

--- popular.php
+++ popular.php
@@ -1,12 +1,12 @@
 <?php
 
-header('Content-type: text/html; charset=utf-8');
-
-require_once("../lib/Parser.class.php");
-
-$Parser = new Parser();
-$Popular = $Parser->get_popular();
-
-print_r($Popular);
+  header('Content-type: text/html; charset=utf-8');
+  
+  require_once("imdb_parser/lib/Parser.class.php");
+  
+  $Parser = new Parser();
+  $Popular = $Parser->get_popular();
+  
+  print_r($Popular);
 
 ?>
