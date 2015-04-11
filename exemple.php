4eb8c0f5f916203aa9311ec66733d574236e8f93
Ismail <xd4rker@gmail.com>
2015-04-11 12:23:14 +0100

    Update exemple.php

--- exemple.php
+++ exemple.php
@@ -1,17 +1,16 @@
 <?php
 
-header('Content-type: text/html; charset=utf-8');
-
-require_once("../lib/Parser.class.php");
-
-$Parser = new Parser("tt0948470");
-$Parser2 = new Parser("The last samurai");
-
-$a = $Parser->get_info();
-$b = $Parser2->get_info();
-
-print_r($a);
-
-print_r($b);
+  header('Content-type: text/html; charset=utf-8');
+  
+  require_once("imdb_parser/lib/Parser.class.php");
+  
+  $Parser1 = new Parser("tt0948470");
+  $Parser2 = new Parser("The last samurai");
+  
+  $a = $Parser1->get_info();
+  $b = $Parser2->get_info();
+  
+  print_r($a);
+  print_r($b);
 
 ?>
